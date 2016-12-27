115f6d396d75ded12b732230538a04de6093ea2f
dell <romakost29@mail.ru>
2016-12-27 21:59:15 +0200

    First commit

---
+++ app/src/main/java/com/example/dell/testprojforgit/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.dell.testprojforgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ((Button) findViewById(R.id.btn_hello)).setOnClickListener(this);
+
+
+
+
+
+    }
+}
