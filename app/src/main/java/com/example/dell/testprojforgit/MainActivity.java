866afdf22460c04c01eb791174d1adfd0e306ec6
dell <romakost29@mail.ru>
2016-12-28 00:07:18 +0200

    add textView

--- app/src/main/java/com/example/dell/testprojforgit/MainActivity.java
+++ app/src/main/java/com/example/dell/testprojforgit/MainActivity.java
@@ -2,25 +2,22 @@ package com.example.dell.testprojforgit;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Toast;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    @Override
+public class MainActivity extends AppCompatActivity /*implements View.OnClickListener*/{
+    // WORK_BR3
+
+    TextView tv;
+    TextView tv2;
+
+   /* @Override
     public void onClick(View v) {
         Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_LONG).show();
-        //Test comment ee//
-    }
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
-
-
     }
 }
